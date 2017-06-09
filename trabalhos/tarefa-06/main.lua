122f62e90d8a7faad410b7b185bf59866b6dc9d5
André Viana Martins Silveira de Souza <andresilveira1992@gmail.com>
2017-06-09 12:02:29 -0300

    add enum, modifiquei monster, ele so comeca com 15
    acrescentei sentido do monster como enum - esquerda, direita
    Modifiquei monster - toda vez que jogador colidir com um monster, e' criado um outro.

--- trabalhos/tarefa-06/main.lua
+++ trabalhos/tarefa-06/main.lua
@@ -14,11 +14,7 @@ Array
 ]]
 chao = {}
 dicImagens = {}
---[[
-Dicionario
-.Conjunto (infinito) nao ordenado de valores heterogeneos
-.Mapeia uma chave para um valor
-]]
+
 for i = 1, 15 do
 	monster[i] = {}
 end
@@ -26,34 +22,42 @@ end
 function love.load()
 	fim = false
 	tela = {comp = love.graphics.getWidth(), altu = love.graphics.getHeight()}
-	dicImagens[chao] = 'resources/chao.png'
-	dicImagens[jogador] = 'resources/jogador.png'
-	dicImagens[bandeira] = 'resources/bandeira.png'
-	dicImagens[monster] = 'resources/monster.png'
-	chao.img = love.graphics.newImage(dicImagens[chao])
-	jogador.img = love.graphics.newImage(dicImagens[jogador])
-	bandeira.img = love.graphics.newImage(dicImagens[bandeira])
+	dicImagens["chao"] = 'resources/chao.png'
+	dicImagens["jogador"] = 'resources/jogador.png'
+	dicImagens["bandeira"] = 'resources/bandeira.png'
+	dicImagens["monster"] = 'resources/monster.png'
+	dicImagens["propMonster"] = 0.3 -- proporcao da imagem do monstro a ser mudada
+	dicImagens["propJogador"] = 0.2
+	dicImagens["propBandeira"] = 0.5
+	chao.img = love.graphics.newImage(dicImagens["chao"])
+	jogador.img = love.graphics.newImage(dicImagens["jogador"])
+	bandeira.img = love.graphics.newImage(dicImagens["bandeira"])
 
 	for i = 1, #monster do
 		monster[i].x = tela.comp * love.math.random()
 		monster[i].y = tela.altu/1.5 * love.math.random()
-		monster[i].img = love.graphics.newImage(dicImagens[monster])
-		monster[i].a = monster[i].img : getHeight()*0.3
-		monster[i].c = monster[i].img : getWidth()*0.3
+		monster[i].img = love.graphics.newImage(dicImagens["monster"])
+		monster[i].a = monster[i].img : getHeight()*dicImagens["propMonster"]
+		monster[i].c = monster[i].img : getWidth()*dicImagens["propMonster"]
 		monster[i].velo = 300*love.math.random()
-		monster[i].sentido = true
+		monster[i].sentido = 'esquerda'
+		--[[
+		Enumeracao
+		.Tipo em que todos os valores possiveis sao constantes enumeradas
+		.Substituem constantes numericas
+		]]
 	end
 
 	jogador.x = tela.comp/2
-	jogador.c = jogador.img : getWidth()*0.2 -- imagem esta diminuida
+	jogador.c = jogador.img : getWidth()*dicImagens["propJogador"] -- imagem esta diminuida
 	jogador.y = tela.altu - 30
-	jogador.a = jogador.img : getHeight()*0.2
+	jogador.a = jogador.img : getHeight()*dicImagens["propJogador"]
 	jogador.velo = 200
 
 	bandeira.x = tela.comp*love.math.random()
 	bandeira.y = 0
-	bandeira.c = bandeira.img : getWidth()*0.5
-	bandeira.a = bandeira.img : getHeight()*0.5
+	bandeira.c = bandeira.img : getWidth()*dicImagens["propBandeira"]
+	bandeira.a = bandeira.img : getHeight()*dicImagens["propBandeira"]
 
 	mensagemGanhou = {'Bandeira conquistada!', tela.comp/4, tela.altu/2, 0, 3, 3} -- com valores acertados para a funcao love.graphics.print
 	--[[
@@ -68,21 +72,22 @@ function love.update(dt)
 	if not fim then
 		for i=1, #monster do
 			if monster[i].x < monster[i].c/2 then
-				monster[i].sentido = false
+				monster[i].sentido = 'direita'
 			end
 			if monster[i].x > tela.comp - monster[i].c/2 then
-				monster[i].sentido = true
+				monster[i].sentido = 'esquerda'
 			end
-			if monster[i].sentido then
+			if monster[i].sentido == 'esquerda' then
 				monster[i].x = monster[i].x - (monster[i].velo*dt)
 			end
-			if not monster[i].sentido then
+			if monster[i].sentido == 'direita' then
 				monster[i].x = monster[i].x + (monster[i].velo*dt)
 			end
 
 			if collides(jogador, monster[i]) then
 				jogador.x = tela.comp/2
 				jogador.y = tela.altu - 30
+				criarMonstro()
 			end	
 
 			if collides(jogador, bandeira) then
@@ -112,11 +117,11 @@ end
 
 function love.draw()
 	love.graphics.draw(chao.img)
-	love.graphics.draw(jogador.img, jogador.x, jogador.y, 0, 0.2, 0.2)
-	love.graphics.draw(bandeira.img, bandeira.x, bandeira.y, 0, 0.5, 0.5)
+	love.graphics.draw(jogador.img, jogador.x, jogador.y, 0, dicImagens["propJogador"], dicImagens["propJogador"])
+	love.graphics.draw(bandeira.img, bandeira.x, bandeira.y, 0, dicImagens["propBandeira"], dicImagens["propBandeira"])
 	
 	for i = 1, #monster do
-		love.graphics.draw(monster[i].img, monster[i].x, monster[i].y, 0, 0.3, 0.3)
+		love.graphics.draw(monster[i].img, monster[i].x, monster[i].y, 0, dicImagens["propMonster"], dicImagens["propMonster"])
 	end
 
 	if fim then
@@ -129,4 +134,15 @@ function collides (a, b)
 		   a.x <= b.x + b.c and 
 		   a.y + a.a >= b.y and 
 		   a.y <= b.y + b.a
+end
+
+function  criarMonstro()
+	monster[#monster + 1] = {}
+	monster[#monster].x = tela.comp * love.math.random()
+	monster[#monster].y = tela.altu/1.5 * love.math.random()
+	monster[#monster].img = love.graphics.newImage(dicImagens["monster"])
+	monster[#monster].a = monster[#monster].img : getHeight()*dicImagens["propMonster"]
+	monster[#monster].c = monster[#monster].img : getWidth()*dicImagens["propMonster"]
+	monster[#monster].velo = 300*love.math.random()
+	monster[#monster].sentido = 'esquerda'
 end
